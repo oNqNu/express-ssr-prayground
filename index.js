962d0198ae82e8cd2baa32b61984974d0b794a73
oNqNu <onanukintaro@gmail.com>
2022-07-09 18:02:57 +0900

    webpackを使って動的な処理をできるようにした

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 // 3000番ポートで鯖を立てる
 app.listen(3000);
 
+app.use(express.static('assets'));
+
 app.get('/', (_, res) => {
   const response = ssr();
   res.send(response);
--- views/ssr.js
+++ views/ssr.js
@@ -1,28 +1,23 @@
-require('core-js/modules/es.object.define-property.js');
+"use strict";
 
-Object.defineProperty(exports, '__esModule', {
-  value: true,
+require("core-js/modules/es.object.define-property.js");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
-exports.default = void 0;
+exports["default"] = void 0;
 
-const _react = _interopRequireDefault(require('react'));
+var _react = _interopRequireDefault(require("react"));
 
-const _server = require('react-dom/server');
+var _server = require("react-dom/server");
 
-const _CountUp = _interopRequireDefault(require('./CountUp'));
+var _CountUp = _interopRequireDefault(require("./CountUp"));
 
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-const ssr = function ssr() {
-  return '\n<html>\n<head>\n  <title>CountUp</title>\n  <meta charset="utf-8"/>\n</head>\n<body>\n  <div id="app">\n    '.concat(
-    (0, _server.renderToString)(
-      /* #__PURE__ */ _react.default.createElement(_CountUp.default, null)
-    ),
-    '\n  </div>\n</body>\n</html>\n'
-  );
+var ssr = function ssr() {
+  return "\n<html>\n<head>\n  <title>CountUp</title>\n  <meta charset=\"utf-8\"/>\n</head>\n<body>\n  <div id=\"app\">\n    ".concat((0, _server.renderToString)( /*#__PURE__*/_react["default"].createElement(_CountUp["default"], null)), "\n  </div>\n  <script src=\"./client.js\"></script>\n</body>\n</html>\n");
 };
 
-const _default = ssr;
-exports.default = _default;
+var _default = ssr;
+exports["default"] = _default;
